3b8de4a1c028e979623d1fc06f9fb322a3a4e34f
Piotr Kalinowski <kali.piotrek@gmail.com>
2014-06-08 17:47:32 +0200

    Minmax algorytm

--- Minmax.hs
+++ Minmax.hs
@@ -3,8 +3,56 @@ import Data.Char
 import Board
 import State
 
+getMove::State->Piece->Int->(State, Int)
+getMove state piece depth = result where
+	result = getBestState (getMinmax (getPossibleStates state piece) piece depth)
+	
+getMinmax::[State]->Piece->Int->[(State, Int)]
+getMinmax [] _ _ = []
+getMinmax (x:xs) piece depth = (x, minmax x piece depth piece) : getMinmax xs piece depth
+	
+getBestState::[(State, Int)]->(State, Int)
+getBestState [x] = x
+getBestState (x:y:ys) 	| (snd x) > (snd y)	= getBestState(x:ys)
+						| otherwise 		= getBestState(y:ys)
+	
+
+minmax::State->Piece->Int->Piece->Int
+minmax state piece k maximizingPlayer = if (isWolfWin state || isSheepsWin state) then getEndGameScore state piece else 
+		if k == 0 then 0 else if maximizingPlayer == piece then getMax (countMinmax (getPossibleStates state piece) piece k maximizingPlayer) else 
+			getMin (countMinmax (getPossibleStates state piece) piece k maximizingPlayer)
+
+getMax::[(State, Int)]->Int
+getMax [x] = snd x
+getMax (x:y:ys) | (snd x) > (snd y)	= getMax (x:ys)
+				| otherwise 		= getMax (y:ys)
+
+getMin::[(State, Int)]->Int
+getMin [x] = snd x
+getMin (x:y:ys) | (snd x) < (snd y)	= getMin (x:ys)
+				| otherwise 		= getMin (y:ys)
+
+countMinmax::[State]->Piece->Int->Piece->[(State, Int)]
+countMinmax [] _ _ _ = [] 
+countMinmax (x:xs) piece k maximizingPlayer = result where
+	player = if piece == Wolf then Sheep else Wolf
+	result = (x, minmax x player (k-1) maximizingPlayer) : countMinmax xs piece k maximizingPlayer
+
+-----------------------------------------------------------------------------------
+getEndGameScore::State->Piece->Int
+getEndGameScore state Sheep = if isSheepsWin state then 1 else if isWolfWin state then -1 else 0 
+
+getEndGameScore state Wolf = if isSheepsWin state then -1 else if isWolfWin state then 1 else 0
+
+isWolfWin::State->Bool
+isWolfWin (x:xs) = if snd(snd x) == 0 then True else False
+
+isSheepsWin::State->Bool
+isSheepsWin s = if length (getPossibleStates s Wolf) == 0 then True else False
+
 getPossibleStates::State->Piece->[State]
 getPossibleStates [] _ = []
+
 getPossibleStates (x:xs) Wolf = getPossibleStates' xs x
 
 getPossibleStates s Sheep = getPossibleStates' ([s !! 0] ++ [s !! 2] ++ [s !! 3] ++ [s !! 4]) (s !! 1) 
