b583cf8d92513c8368ab56c487916ef2bfc398f3
Mateusz Statkiewicz <mat.statkiewicz@gmail.com>
2014-06-08 15:22:20 +0200

    State validations

--- Board.hs
+++ Board.hs
@@ -11,21 +11,21 @@ type Square = (Maybe PieceType,SqColor)
 type Board = [[Square]]
 
 setState::Board->State->Board
-	setState b [] = b
-	setState b (p:st) = 
-		updateMatrix (normalizePosition (getPosition p)) (Just (getType p), Black) (setState b st)
+setState b [] = b
+setState b (p:st) = 
+	updateMatrix (normalizePosition (getPosition p)) (Just (getType p), Black) (setState b st)
 
 -- **************** output functions *******************
 prettyBoard::Board->String
-	prettyBoard  = unlines . map (concatMap prettySquare)
+prettyBoard  = unlines . map (concatMap prettySquare)
 
 prettyBoardIndent::Int->Board->String
-	prettyBoardIndent x = ('\n':) . concatMap ((('\n':take x (repeat ' '))++) . concatMap prettySquare)
+prettyBoardIndent x = ('\n':) . concatMap ((('\n':take x (repeat ' '))++) . concatMap prettySquare)
 
 prettySquare::Square->String
-	prettySquare (Nothing, Black) = "#"
-	prettySquare (Nothing, White) = " "
-	prettySquare (Just a, _) = show a
+prettySquare (Nothing, Black) = "#"
+prettySquare (Nothing, White) = " "
+prettySquare (Just a, _) = show a
 
 -- **************** some boards *******************
 initialBoard,emptyBoard::Board
@@ -33,11 +33,11 @@ initialBoard,emptyBoard::Board
 initialBoard = setState emptyBoard initialState
 
 emptyBoard =  [[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
-					[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
-					[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
-					[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
-					[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
-					[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
-					[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
-					[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)]]
+				[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
+				[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
+				[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
+				[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
+				[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)],
+				[(Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black)],
+				[(Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White), (Nothing, Black), (Nothing, White)]]
 
--- State.hs
+++ State.hs
@@ -1,48 +1,69 @@
 module State where
+import Utils
 
 -- **************** data types *******************
 
 data PieceType = Wolf | Sheep deriving Eq
-	instance Show PieceType where
-		show Wolf  = "W"
-		show Sheep = "S"
+instance Show PieceType where
+	show Wolf  = "W"
+	show Sheep = "S"
 
 -- przyjmujemy zwykły układ współrzędnych - (0,0) jest w lewym-dolnym rogu, pola numerowane od 0
 type Pos = (Int, Int)
 
 -- zamiana na układ współrzędnych gdzie (0,0) jest w lewym-górnym rogu, na potrzeby wyświetlania
-	normalizePosition:: Pos->Pos
-		normalizePosition (x, y) = (7-x, y) 
+normalizePosition:: Pos->Pos
+normalizePosition (x, y) = (7-x, y) 
 
 type Piece = (PieceType,Pos)
-
-	getPosition::Piece->Pos
-		getPosition (_,pos) = pos
-
-	getType::Piece->PieceType
-		getType (t,_) = t
+-- zwraca pozycje pionka
+getPosition::Piece->Pos
+getPosition (t,pos) = pos
+-- zwraca typ pionka
+getType::Piece->PieceType
+getType (t,pos) = t
 
 type State = [Piece]
+-- zamienia stan na ciąg znaków
+toString::State->String
+toString [] = []
+toString (x:xs) = show x ++ " | " ++ toString xs
 
-	toString::State->String
-		toString [] = []
-		toString (x:xs) = show x ++ " | " ++ toString xs
+toString'::[State]->String
+toString' [] = []
+toString' (x:xs) = toString x ++ " ;; " ++ toString' xs
 
-	toString'::[State]->String
-		toString' [] = []
-		toString' (x:xs) = toString x ++ " ;; " ++ toString' xs
+-- przygotowuje stan do zapisania do pliku
+toText::State->String
+toText [] = []
+toText (x:xs) = show x ++ "\n" ++ toText xs
 
-	toText::State->String
-		toText [] = []
-		toText (x:xs) = show x ++ "\n" ++ toText xs
+-- porusza itym pionkiem o pos 
+move::State->Int->Pos->State
+move [] i pos = []
+move (s:sn) 0 pos = [((getType s),(addPair (getPosition s) pos))]++sn
+move (s:sn) i pos = [s] ++ (move sn (i-1) pos)
+
+--sprawdza czy pozycja jest poprawna
+isPositionValid::Pos->Bool
+isPositionValid (x,y) = (x>=0) && (x<=7) && (y>=0) && (y<=7)
+ 
+--sprawdza czy pozycja jest zajeta przez inny pionek
+isFree::Pos->State->Bool
+isFree _ [] = True
+isFree pos (s:sn) = (pos!=(getPosition s)) && isFree pos sn
+
+--sprawdza czy stan jest poprawny
+isValid::State->Bool
+isValid [] = True
+isValid (s:sn) = (isPositionValid (getPosition s)) && (isFree (getPosition s) sn) && (isValid sn)
 
 -- **************** some states *******************
-
 initialState::State
-	initialState = [(Wolf,(0,0)),(Sheep,(7,1)),(Sheep,(7,3)),(Sheep,(7,5)),(Sheep,(7,7))]
+initialState = [(Wolf,(0,0)),(Sheep,(7,1)),(Sheep,(7,3)),(Sheep,(7,5)),(Sheep,(7,7))]
 
 onePieceState::State
-	onePieceState = [(Wolf,(0,0))]
+onePieceState = [(Wolf,(0,0))]
 
 testState::State
-	testState = [(Wolf,(1,5)),(Sheep,(2,2)),(Sheep,(2,4)),(Sheep,(4,2)),(Sheep,(4,4))]
+testState = [(Wolf,(1,5)),(Sheep,(2,2)),(Sheep,(2,4)),(Sheep,(4,2)),(Sheep,(4,4))]
